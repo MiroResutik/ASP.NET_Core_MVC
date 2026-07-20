2492ecc0d0cc81caf449d9f36ff7846c174631e2
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-07-20 09:44:23 +0100

    Category CRUD operations added

--- WebMagazines/WebMagazines/Controllers/CategoryController.cs
+++ WebMagazines/WebMagazines/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebMagazines.Data;
+using WebMagazines.Models;
 
 namespace WebMagazines.Controllers
 {
@@ -18,5 +19,104 @@ namespace WebMagazines.Controllers
             var categories = _context.Categories.ToList();
             return View("Index", categories);
         }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Add this attribute to protect against Cross-Site Request Forgery (CSRF) attacks
+        [ActionName("Create")] // Specify the action name for the POST method
+        public IActionResult CreatePOST(Category category)
+        {
+            if (!String.IsNullOrEmpty(category.Name) && _context.Categories.Any(c => c.Name.ToLower() == category.Name.ToLower()))
+            {
+                ModelState.AddModelError("", "Category name already exists!!!");
+                //return View(category);
+            }
+            if (ModelState.IsValid)
+            {
+
+                // Add the new category to the database using the ApplicationDbContext 
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View(category);
+        }
+        public IActionResult Update(int? id)
+        {
+            if(id== null || id == 0)
+            {
+                return NotFound();
+            }
+            // Retrieve the category from the database using the ApplicationDbContext
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Add this attribute to protect against Cross-Site Request Forgery (CSRF) attacks
+        [ActionName("Update")] // Specify the action name for the POST method
+        public IActionResult UpdatePOST(Category category)
+        {
+            if (!String.IsNullOrEmpty(category.Name) && _context.Categories.Any(c => c.Name.ToLower() == category.Name.ToLower() && c.Id != category.Id))
+            {
+                ModelState.AddModelError("", "Category name already exists!!!");
+                //return View(category);
+            }
+            if (ModelState.IsValid)
+            {
+
+                // Add the new category to the database using the ApplicationDbContext 
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View(category);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            // Retrieve the category from the database using the ApplicationDbContext
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Add this attribute to protect against Cross-Site Request Forgery (CSRF) attacks
+        [ActionName("Delete")] // Specify the action name for the POST method
+        public IActionResult DeletePOST(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+        
     }
 }
--- WebMagazines/WebMagazines/Models/Category.cs
+++ WebMagazines/WebMagazines/Models/Category.cs
@@ -6,8 +6,13 @@ namespace WebMagazines.Models
     {
         //[Key]
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; } = string.Empty;
 
-        public int DisplayOrder { get; set; }
+        [Required]
+        // Add validation for display order to be between 0 and 100
+        [Range(0, 100, ErrorMessage ="Display order must be between 0 and 100!!!")] 
+        public int? DisplayOrder { get; set; }
     }
 }
