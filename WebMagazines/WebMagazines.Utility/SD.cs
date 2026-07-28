3220bec4af3e6bae3b503eeed8453cfa7b1333ad
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-07-28 15:32:17 +0100

    Identity - Roles added

---
+++ WebMagazines/WebMagazines.Utility/SD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebMagazines.Utility
+{
+    public class SD
+    {
+
+        public const string RoleCustomer = "Customer";
+        public const string RoleAdmin = "Admin";
+        public const string RoleEmployee = "Employee";
+    }
+}
