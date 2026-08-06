e73597c09f876847c0f0fe6a1c435c2b86d1ad64
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-08-06 08:46:16 +0100

    Cart Controller - IndexPost Action and Order Confirmation

--- WebMagazines/WebMagazines/Areas/Customer/Controllers/CartController.cs
+++ WebMagazines/WebMagazines/Areas/Customer/Controllers/CartController.cs
@@ -5,6 +5,7 @@ using System.Security.Claims;
 using WebMagazines.Businness.Services.IServices;
 using WebMagazines.Models;
 using WebMagazines.Models.ViewModels;
+using WebMagazines.Utility;
 
 
 namespace WebMagazines.Areas.Customer.Controllers
@@ -13,24 +14,31 @@ namespace WebMagazines.Areas.Customer.Controllers
     [Authorize] // Ensure that only authenticated users can access the CartController]
     public class CartController : Controller
     {
-        // Dependency injection of the IProductService and IShoppingCartService through the constructor
+        // Dependency injection of the IProductService,IShoppingCartService
+        // IApplicationUserService, and IOrderService through the constructor 
         private readonly IProductService _productService;
         private readonly IShoppingCartService _shoppingCartService;
         private readonly IApplicationUserService _applicationUserService;
+        private readonly IOrderService _orderService;
 
-        // Cart Constructor to inject the IProductService and IShoppingCartService dependencies
-        public CartController(IProductService productService, IShoppingCartService shoppingCartService, IApplicationUserService applicationUserService)
+        // Cart Constructor to inject the IProductService,IShoppingCartService,
+        // IApplicationUserService, and IOrderService dependencies
+        public CartController(IProductService productService, IShoppingCartService shoppingCartService, IApplicationUserService applicationUserService, IOrderService orderService)
         {
             _productService = productService;
             _shoppingCartService = shoppingCartService;
             _applicationUserService = applicationUserService;
+            _orderService = orderService;
         }
 
+        // Action method to display the shopping cart for the authenticated user
         public async Task<IActionResult> Index()
         {
+            // Retrieve the userId from the authenticated user's claims
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var userId = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
+            // If the userId is null or empty, redirect the user to the login page
             if (string.IsNullOrEmpty(userId))
             {
                 return Unauthorized();
@@ -61,22 +69,82 @@ namespace WebMagazines.Areas.Customer.Controllers
             shoppingCartVM.OrderHeader.State = user.State;
             shoppingCartVM.OrderHeader.PostalCode = user.PostalCode;
 
+            // Calculate the total order amount based on the cart items and their quantities
             foreach (var cart in shoppingCartVM.ShoppingCartList)
             {
                 //cart.Product = await _productService.GetProductByIdAsync(cart.ProductId);
                 shoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
             }
 
-            return View(shoppingCartVM);
+            return View(shoppingCartVM); // Return the ShoppingCartVm instance to the view for rendering
+        }
+
+        // Action method to handle the POST request for the shopping cart form submission
+        [HttpPost] // Handle the POST request for the shopping cart form submission
+        [ActionName("Index")] // Specify the action name for the POST request to match the GET request
+        public async Task<IActionResult> IndexPost(ShoppingCartVM shoppingCartVM)
+        {
+            // Retrieve the userId from the authenticated user's claims
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Unauthorized();
+                //return RedirectToAction("Login", "Account", new { area = "Identity" });
+            }
+
+            // Retrieve the shopping cart items for the authenticated user using the userId
+            var cartItems = await _shoppingCartService.GetUserCartItemsAsync(userId);
+
+            // Populate the ShoppingCartVM with the retrieved cart items and user details
+            shoppingCartVM.ShoppingCartList = cartItems;
+            shoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
+            shoppingCartVM.OrderHeader.ApplicationUserId = userId;
+     
+
+            // Calculate the total order amount based on the cart itmes and their quantities
+            foreach (var cart in shoppingCartVM.ShoppingCartList)
+            {
+                shoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+            }
+
+            // Set the order status to "Approved" for the new order
+            shoppingCartVM.OrderHeader.OrderStatus = SD.StatusApproved;
+
+            // Create a list of OrderDetails based on the shopping cart items using projection with LINQ
+            shoppingCartVM.OrderHeader.OrderDetails = shoppingCartVM.ShoppingCartList
+                .Select(cart => new OrderDetails
+            {
+                ProductId = cart.ProductId,
+                Count = cart.Count,
+                Price = cart.Price
+            }).ToList();
+
+            // Create Order
+            // Call the CreateOrderAsync method of the IOrderService to create a new order based on the ShoppingCartVM
+            await _orderService.CreateOrderAsync(shoppingCartVM.OrderHeader);
+
+            // Clear the shopping cart for the user after creating the order
+            // await _shoppingCartService.ClearUserCartAsync(userId);
+
+            // Redirect the user to the OrderConfirmation action with the order ID as a route parameter
+            return RedirectToAction("OrderConfirmation", new { id = shoppingCartVM.OrderHeader.Id });
+        }
+
+        // Action method to display the order confirmation page after a successful order placement
+        public async Task<IActionResult> OrderConfirmation(int id)
+        {
+            return View(id);
         }
 
         // Action method to increase the quantity of an item in the shopping cart
         public async Task<IActionResult> Plus(int cartId)
         {
-
             // Retrieve the cart item using the cartId
             var cart = await _shoppingCartService.GetCartByIdAsync(cartId);
 
+            // If the cart item exists, increase the count of the cart item and save the changes
             if (cart != null)
             {
                 // Ensure that the count does not exceed 1000
@@ -84,24 +152,26 @@ namespace WebMagazines.Areas.Customer.Controllers
                 {
                     // Optionally, you can display a message to the user indicating that the maximum quantity has been reached
                     TempData["ErrorMessage"] = "Maximum quantity reached for this item.";
-
                 }
                 else
                 {
                     // Increase the count of the cart item and save the changes
                     cart.Count++;
-                    await _shoppingCartService.UpdateCartAsync(cart);
+                    
+                    await _shoppingCartService.UpdateCartAsync(cart); // Update the cart item in the database
                 }
-                
-
             }
 
             return RedirectToAction(nameof(Index));
         }
+
         // Action method to decrease the quantity of an item in the shopping cart
         public async Task<IActionResult> Minus(int cartId)
         {
+            // Retrieve the cart item using the cartId
             var cart = await _shoppingCartService.GetCartByIdAsync(cartId);
+
+            // If the cart item exists, decrease the count of the cart item and save the changes
             if (cart != null)
             {
                 cart.Count--;
@@ -131,13 +201,15 @@ namespace WebMagazines.Areas.Customer.Controllers
         // Action method to update an item in the shopping cart
         public async Task<IActionResult> UpdateCart(int cartId, int count)
         {
+            // Retrieve the cart item using the cartId
             var cart = await _shoppingCartService.GetCartByIdAsync(cartId);
+
             if (cart == null)
             {
                 return NotFound();
             }
 
-
+            // Update the count of the cart item based on the provided count value
             if (count <= 1)
             {
                 cart.Count = 0;
@@ -153,9 +225,10 @@ namespace WebMagazines.Areas.Customer.Controllers
                     cart.Count = count;
                 }
             }
-            await _shoppingCartService.UpdateCartAsync(cart);
+
+            await _shoppingCartService.UpdateCartAsync(cart); // Update the cart item in the database
             //await UpdateCartSessionAsync();
-            return Ok(new { success = true });
+            return Ok(new { success = true }); // Return a JSON response indicating success
         }
     }
 }
