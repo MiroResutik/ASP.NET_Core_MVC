4556779d6df9ed54902450f387e79dbc9eb7b3a4
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-08-10 09:02:45 +0100

    Filter by Order status (JavaScript) - Order Status Constants

--- WebMagazines/WebMagazines.Utility/SD.cs
+++ WebMagazines/WebMagazines.Utility/SD.cs
@@ -4,11 +4,23 @@ using System.Text;
 
 namespace WebMagazines.Utility
 {
+    // Static class to hold constant values
     public class SD
     {
-
+        // Role Constants
         public const string RoleCustomer = "Customer";
         public const string RoleAdmin = "Admin";
         public const string RoleEmployee = "Employee";
+
+
+        // Order Status Constants
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusProcessing = "Processing";
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+        public const string StatusCompleted = "Completed";
+
     }
 }
