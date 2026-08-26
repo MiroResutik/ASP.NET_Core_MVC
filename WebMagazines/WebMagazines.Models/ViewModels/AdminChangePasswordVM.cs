a34085c3f0f6ef3c6ffa18bfb85106e38ce8e301
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-08-26 09:32:18 +0100

    Admin Change Password ViewModel

---
+++ WebMagazines/WebMagazines.Models/ViewModels/AdminChangePasswordVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WebMagazines.Models.ViewModels
+{
+    public class AdminChangePasswordVM
+    {
+        public string UserId { get; set; }
+
+        [Display(Name = "User Email")]
+        public string UserEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
