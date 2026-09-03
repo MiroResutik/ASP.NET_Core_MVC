1df8048b287660a71ccac9bafcf805e6240927f4
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-09-03 08:25:04 +0100

    Dashboard Controller - Order, Product Count and User Count for index page

--- WebMagazines/WebMagazines/Areas/Admin/Controllers/DashboardController.cs
+++ WebMagazines/WebMagazines/Areas/Admin/Controllers/DashboardController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebMagazines.DataAccess.Data;
+using WebMagazines.Models.ViewModels;
 using WebMagazines.Utility;
 
 namespace WebMagazines.Areas.Admin.Controllers
@@ -8,9 +11,31 @@ namespace WebMagazines.Areas.Admin.Controllers
     [Authorize(Roles = SD.RoleAdmin + "," + SD.RoleEmployee)]
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public DashboardController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            var orders = await _db.OrderHeaders.ToListAsync();
+            var productCount = await _db.Products.CountAsync();
+            var userCount = await _db.Users.CountAsync();
+
+            DashboardVM dashboardVM = new ()
+            {
+                TotalOrders = orders.Count,
+                TotalProducts = productCount,
+                TotalUsers = userCount,
+                TotalRevenue = orders.Where(o => o.OrderStatus == SD.StatusApproved || o.OrderStatus == SD.StatusShipped)
+                .Sum(o => o.OrderTotal)
+            };
+
+            return View(dashboardVM);
+        }
+
+
     }
 }
