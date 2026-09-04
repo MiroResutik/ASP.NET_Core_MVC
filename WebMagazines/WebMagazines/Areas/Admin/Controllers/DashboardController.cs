69a9d29171b8b2321f8aa9b9b73393c4e02450d3
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-09-04 09:23:01 +0100

    Get Revenue chart data Endpoint

--- WebMagazines/WebMagazines/Areas/Admin/Controllers/DashboardController.cs
+++ WebMagazines/WebMagazines/Areas/Admin/Controllers/DashboardController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebMagazines.DataAccess.Data;
+using WebMagazines.Models;
 using WebMagazines.Models.ViewModels;
 using WebMagazines.Utility;
 
@@ -36,6 +37,35 @@ namespace WebMagazines.Areas.Admin.Controllers
             return View(dashboardVM);
         }
 
+        // Endpoint to get the total revenue for last 30 days
+        [HttpGet]
+        public async Task<IActionResult> GetChartData()
+        {
+            // Retrieve the necessary data for the chart
+            var orders = await _db.OrderHeaders.ToListAsync();
+            var productCount = await _db.Products.Include(p => p.Category).ToListAsync();
+            var categories = await _db.Categories.ToListAsync();
 
+            // Revenue by month - last 6 months
+            var now = DateTime.UtcNow;
+            var sixMonthsAgo = now.AddMonths(-5);
+            var monthlyRevenue = Enumerable.Range(0, 6).Select(i =>
+            {
+                var month = sixMonthsAgo.AddMonths(i);
+                var revenue = orders
+                .Where(o => o.OrderDate.Year == month.Year && o.OrderDate.Month == month.Month
+                && (o.OrderStatus == SD.StatusApproved || o.OrderStatus == SD.StatusShipped))
+                .Sum(o => o.OrderTotal);
+
+                return new { Label = month.ToString("MMM yyyy"), Revenue = revenue };
+            }).ToList();
+
+
+            return Json(new
+            {
+                monthlyRevenue,
+
+            });
+        }
     }
 }
